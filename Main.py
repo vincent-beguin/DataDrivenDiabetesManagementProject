d08405a883259df27f660d61062226bc0c4cf3a1
vincent <vincent.beguin@students.unibe.ch>
2024-11-11 13:09:02 +0100

    modified:   Main.py
    added data split to main, if the split code doesn't change, TrainTestSplit.py might get removed from the project. Otherwise, if the splitting code gets longer, it should get moved to TrainTestSpilt.py

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ import sklearn as skl
 data = np.genfromtxt("diabetes.csv",delimiter=',')
 
 x_data = data[1:, :7]
-y_data = data[1:, 8]
+y_data = data[1:,  8]
 
-print(x_data)
-print(y_data)
+#Split the data into a training set and a validation set
 
+x_train, x_valid, y_train, y_valid = skl.train_test_split(x_data, y_data, test_size=0.2, random_state=1) #80-20 split. random_state is set to 1 to keep repeatability between tests (for now).
