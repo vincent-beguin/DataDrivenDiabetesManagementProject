4de9b07ec3cf8781cb6488ebbb1ca967b5f33014
vincent-beguin <vincent.beguin@students.unibe.ch>
2024-12-15 15:36:20 +0100

    Delete DataNormalisation.py

--- DataNormalisation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-norm_factors = np.array([1/20, 1/200, 1/150, 1/100, 1/1000, 1/100, 1/3, 1/100])
-
-def data_normalisation(data):
-    norm_matrix = np.diagonal(norm_factors)
-    norm_data = np.matmul(norm_matrix, data)
-    return norm_data
